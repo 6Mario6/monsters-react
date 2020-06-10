b6ee676d06c60e2d519cefeaa8da350da52f4591
Mario <jorge.fernandez@pragma.com.co>
2020-06-09 23:47:01 -0500

    Updates

---
+++ precache-manifest.a03d01dfe3e3eab3bacb853b7fb12a7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c9bbd515150212c4b445c4313d88df5",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "55a24c2a6be41b2797cd",
+    "url": "/monsters-react/static/css/main.ef169fd5.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-react/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-react/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55a24c2a6be41b2797cd",
+    "url": "/monsters-react/static/js/main.22a759c7.chunk.js"
+  },
+  {
+    "revision": "bb10d4d4a0632a4e8caf",
+    "url": "/monsters-react/static/js/runtime-main.2624874f.js"
+  }
+]);
